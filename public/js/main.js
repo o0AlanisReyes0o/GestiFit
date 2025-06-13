35f67e1358543c23891f30b4043cdef7c0083233
stebancito <192398777+stebancito@users.noreply.github.com>
2025-06-13 15:24:30 -0600

    E: Login agregado y sutlies cambios en anclas para enlaces

--- public/js/main.js
+++ public/js/main.js
@@ -142,6 +142,21 @@
         return false;
     });
 
+        // Toggle password visibility
+    document.querySelectorAll('.toggle-password').forEach(button => {
+        button.addEventListener('click', function() {
+            const passwordInput = this.previousElementSibling;
+            const icon = this.querySelector('i');
+            
+            if (passwordInput.type === 'password') {
+                passwordInput.type = 'text';
+                icon.classList.replace('fa-eye', 'fa-eye-slash');
+            } else {
+                passwordInput.type = 'password';
+                icon.classList.replace('fa-eye-slash', 'fa-eye');
+            }
+        });
+    });
 
 })(jQuery);
 
